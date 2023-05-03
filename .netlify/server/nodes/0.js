067880adeb2fcb50e4578e4cf85a47b15e0f0b92
Chadwick Macmillan <chadwickmacmillan@gmail.com>
2023-05-03 19:19:05 -0400

    Safari bar dark + light theme

--- .netlify/server/nodes/0.js
+++ .netlify/server/nodes/0.js
@@ -4,6 +4,6 @@ export const index = 0;
 export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
 export { universal };
 export const universal_id = "src/routes/+layout.js";
-export const imports = ["_app/immutable/entry/_layout.svelte.7b091381.js","_app/immutable/chunks/index.44555a08.js","_app/immutable/chunks/stores.c37ab218.js","_app/immutable/chunks/singletons.7639a212.js","_app/immutable/entry/_layout.js.10e18ea9.js","_app/immutable/chunks/_layout.30c4a981.js"];
+export const imports = ["_app/immutable/entry/_layout.svelte.585e35f0.js","_app/immutable/chunks/index.44555a08.js","_app/immutable/chunks/stores.c375c0a8.js","_app/immutable/chunks/singletons.08a07ab8.js","_app/immutable/entry/_layout.js.10e18ea9.js","_app/immutable/chunks/_layout.30c4a981.js"];
 export const stylesheets = ["_app/immutable/assets/_layout.f46d9adb.css"];
 export const fonts = [];
--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -2,6 +2,6 @@
 
 export const index = 1;
 export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/entry/error.svelte.3fc12239.js","_app/immutable/chunks/index.44555a08.js","_app/immutable/chunks/stores.c37ab218.js","_app/immutable/chunks/singletons.7639a212.js"];
+export const imports = ["_app/immutable/entry/error.svelte.5593ae19.js","_app/immutable/chunks/index.44555a08.js","_app/immutable/chunks/stores.c375c0a8.js","_app/immutable/chunks/singletons.08a07ab8.js"];
 export const stylesheets = [];
 export const fonts = [];
